63338815c5b9631a4dd400d920c6376e30fd927e
Anilghimire10 <117901606+Anilghimire10@users.noreply.github.com>
2024-06-27 11:34:37 +0545

    Revert "Grand master oogway"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,11 +5,10 @@ import Gigs from "./pages/Gigs/Gigs";
 import Gig from "./pages/Gig/Gig";
 import Add from "./pages/Add/Add";
 import Orders from "./pages/Orders/Orders";
-import Profile from "./pages/profile/Profile";
 import Messages from "./pages/Messages/Messages";
 import Message from "./pages/Message/Message";
 import MyGigs from "./pages/MyGigs/MyGigs";
-import { Payment } from "./pages/payment/Payment";
+
 import React from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
@@ -55,14 +54,6 @@ function App() {
         {
           path: "/orders",
           element: <Orders />,
-        },
-        {
-          path: "/profile",
-          element: <Profile />,
-        },
-        {
-          path: "/payment",
-          element: <Payment />,
         },
         {
           path: "/mygigs",
